f56b7b2a3750a380e5f0ca8773095a0ec316bc9c
Dave <DavidRockin@users.noreply.github.com>
2019-11-23 14:25:22 -0500

    login requests

--- components/MainBodyTabs/MainBodyScreensComponents/LoginScreen.js
+++ components/MainBodyTabs/MainBodyScreensComponents/LoginScreen.js
@@ -1,12 +1,30 @@
 import React, { Component, useState } from 'react';
 import { Text, View, Alert } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
+import axios from 'axios'
+import config from '../../../config/app'
 
 import UiStyles from '../../Styles/ui'
 
 const LoginScreen = ({ callback, switchScreens }) => {
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
+
+    function sendRequest() {
+        axios.post(config.endpoint + `/api/login`, {
+            email, password
+        })
+        .then(({ data }) => {
+            if (data.error) {
+                throw new Error('Invalid login request, please try again')
+            }
+            callback()
+        })
+        .catch(err => {
+            console.log(err)
+            Alert.alert('Error', err.statusText || err.toString())
+        })
+    }
 
     return (
         <View style={{ flex: 1, backgroundColor: '#1a78cf' }}>
@@ -15,9 +33,9 @@ const LoginScreen = ({ callback, switchScreens }) => {
                 <Text style={{ color: '#fff', opacity: 0.80, fontSize: 20, textAlign: 'center' }}>make the world a better place</Text>
             </View>
             <View style={{ padding: 48, justifyContent: 'center', alignItems: 'center' }}>
-                <TextInput value={email} style={{ width: '100%', marginBottom: 20 }} placeholder='email address' />
-                <TextInput value={password} style={{ width: '100%', marginBottom: 25 }} placeholder='password' />
-                <Button onPress={callback} style={UiStyles.uiButton}>Sign in</Button>
+                <TextInput value={email} style={{ width: '100%', marginBottom: 20 }} placeholder='email address' onChangeText={setEmail} />
+                <TextInput value={password} style={{ width: '100%', marginBottom: 25 }} placeholder='password' onChangeText={setPassword} />
+                <Button onPress={sendRequest} style={UiStyles.uiButton}>Sign in</Button>
                 <Button onPress={switchScreens} style={UiStyles.uiButtonAlt}>Register an Account</Button>
             </View>
         </View>
